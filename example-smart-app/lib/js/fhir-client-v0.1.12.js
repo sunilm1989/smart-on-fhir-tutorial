9e0ea8ddf807ab1597b6a33a54f4ba52fa9bf8b9
sunilm1989 <41150053+sunilm1989@users.noreply.github.com>
2023-03-23 22:30:54 +0530

    Update fhir-client-v0.1.12.js

--- example-smart-app/lib/js/fhir-client-v0.1.12.js
+++ example-smart-app/lib/js/fhir-client-v0.1.12.js
@@ -17426,8 +17426,7 @@ BBClient.authorize = function(params, errback){
       "scope="+encodeURIComponent(client.scope)+"&"+
       "redirect_uri="+encodeURIComponent(client.redirect_uri)+"&"+
       "state="+encodeURIComponent(state)+"&"+
-      "aud="+encodeURIComponent(params.server)+"&"+
-      "cerner_identifier=123457678";
+      "aud="+encodeURIComponent(params.server)+"&"+"cerner_identifier="+encodeURIComponent('123457678');
     
     if (typeof client.launch !== 'undefined' && client.launch) {
        redirect_to += "&launch="+encodeURIComponent(client.launch);
